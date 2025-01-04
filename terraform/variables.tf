d6a200820cb8801330fbb53d1154f6b4d3647b22
iliapek <127666478+iliapek@users.noreply.github.com>
2025-01-04 23:02:00 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,9 @@
+variable "client_id" {
+  description = "Client ID for the Azure service principal"
+  type        = string
+}
 
+variable "client_secret" {
+  description = "Client secret for the Azure service principal"
+  type        = string
+}
