6eda7908afb9352a8c239a56a0c27e137fe32550
iliapek <127666478+iliapek@users.noreply.github.com>
2025-01-05 17:40:27 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,42 @@
-output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
 
-output "acr_name" {
-  value = azurerm_container_registry.acr.name
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
 }
 
-output "aks_kubeconfig" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config[0].raw_kube_config
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "client_key" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
+
+output "cluster_password" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
+  sensitive = true
+}
+
+output "cluster_username" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
+  sensitive = true
+}
+
+output "host" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
 }
