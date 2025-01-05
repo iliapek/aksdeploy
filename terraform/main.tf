eb3ec92ad8adf64edeb9e5e94e59233e7761bd19
iliapek <127666478+iliapek@users.noreply.github.com>
2025-01-05 18:12:37 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Output the kubeconfig file location
 output "kubeconfig" {
-  value = azurerm_kubernetes_cluster.aks.kube_config.0.raw_kube_config
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
+
+
 
